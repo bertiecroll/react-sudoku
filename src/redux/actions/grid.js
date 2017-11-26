82e30712da1c979d10cb243df3b0ce6e054d5d35
Bertie Croll <bert.croll@gmail.com>
2017-11-26 13:04:01 +0000

    Add addPencilMark and removePencilMark actions

--- src/redux/actions/grid.js
+++ src/redux/actions/grid.js
@@ -1,8 +1,15 @@
-import { UPDATE_SELECTED_CELL, UPDATE_PUZZLE_CELL } from 'redux/actionTypes'
+import { UPDATE_SELECTED_CELL_ID, MARK_CELL_COMPLETED, ADD_PENCIL_MARK, REMOVE_PENCIL_MARK } from 'redux/actionTypes'
 
-export const updateSelectedCell = (cell) => (
-  { type: UPDATE_SELECTED_CELL, cell }
+export const updateSelectedCellId = cellId => (
+  { type: UPDATE_SELECTED_CELL_ID, cellId }
 )
-export const updatePuzzleCell = number => (
-  { type: UPDATE_PUZZLE_CELL, number }
+export const markCellCompleted = cellId => (
+  { type: MARK_CELL_COMPLETED, cellId }
+)
+export const addPencilMark = (cellId, value) => (
+  { type: ADD_PENCIL_MARK, cellId, value }
+)
+
+export const removePencilMark = (cellId, value) => (
+  { type: REMOVE_PENCIL_MARK, cellId, value }
 )
