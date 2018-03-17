51f6ece7f7570c9ca4bd08edbd6ea325091b977b
Bertie Croll <bert.croll@gmail.com>
2018-03-17 15:21:16 +0000

    Add links to AppFooter

--- src/containers/App/AppFooter.js
+++ src/containers/App/AppFooter.js
@@ -3,9 +3,14 @@ import React from 'react'
 export function AppFooter () {
   return (
     <footer className="AppFooter">
-      <a>Create React App</a>
-      <a>Redux</a>
-      <a>React Router</a>
+      <div className="footer-links">
+        <a href="https://github.com/bertiecroll" target="_blank" rel="noopener noreferrer">Created by Bertie Croll</a>
+      </div>
+      <div className="footer-links">
+        <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>
+        <a href="https://github.com/ReactTraining/react-router" target="_blank" rel="noopener noreferrer">React Router</a>
+        <a href="https://github.com/reactjs/redux" target="_blank" rel="noopener noreferrer">Redux</a>
+      </div>
     </footer>
   )
 }
